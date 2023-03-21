cc1beb2e5d19cea9bdf4872f4989fc7dcd2f4fc7
אליה פיקאר <eliya688@gmail.com>
2023-03-21 15:18:24 +0200

    add css

--- script.js
+++
@@ -1,3 +0,0 @@
-function openNewWindow() {
-  var newWindow = window.open('hours.html', 'hours', 'width=400,height=300');
-}
---
+++ static/script.js
@@ -0,0 +1,3 @@
+function openNewWindow() {
+  var newWindow = window.open('hours.html', 'hours', 'width=400,height=300');
+}
